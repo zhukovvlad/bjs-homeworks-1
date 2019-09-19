84688211c811b85a47c057d6b153db1a5a6904c5
zhukovvlad <zhukovvlad@yandex.ru>
2019-09-19 19:11:16 +0300

    Update task.js

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -10,9 +12,46 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-
+  
     // код для задачи №1 писать здесь
     //return totalAmount;
+  let fullPercent = parseFloat(percent);
+  let fullContribution = parseFloat(contribution);
+  let fullAmount = parseFloat(amount);
+  let fullDate = new Date(date);
+  let today = new Date();
+  
+  if (isNaN(fullPercent) || fullPercent < 0) {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}".`;
+  } else if (isNaN(fullContribution) || fullContribution < 0) {
+    return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}".`;
+  } else if (isNaN(fullAmount) || fullAmount < 0) {
+    return `Параметр "Общая стоимость" содержит неправильное значение "${amount}".`;
+  } else if (fullAmount < fullContribution) {
+    return `Кажется, что ипотека Вам не нужна. Стоимость объекта составляет ${amount} рублей, но у Вас уже есть ${contribution} рублей.`;
+  } else if (isNaN(fullDate) || fullDate < today) {
+    return `Параметр "Срок ипотеки" содержит неправильное значение "${date}".`;
+  }
+  //
+  let timeInMonths = (fullDate.getFullYear() - today.getFullYear()) * 12 + Math.abs(fullDate.getMonth() - today.getMonth());
+  
+  let monthlyPercent = percent / (12*100);
+  
+  let requiredCredit = amount - contribution;
+  
+  let monthlyPayment;
+
+  if (monthlyPercent === 0) {
+    monthlyPayment = requiredCredit / timeInMonths;
+  } else {
+    monthlyPayment = requiredCredit * (monthlyPercent + monthlyPercent / (((1+monthlyPercent)**timeInMonths - 1)));
+  }
+
+  let totalAmount = monthlyPayment * timeInMonths;
+  
+  console.log(totalAmount.toFixed(2));
+  
+  return totalAmount.toFixed(2);
 }
 
 function sayHello() {
@@ -25,4 +64,12 @@ function sayHello() {
 function getGreeting(name) {
     // код для задачи №2 писать здесь
     //return greeting;
+  console.log(typeof(name));
+  
+  if (name === "" || name === 'null' || name === 'undefined' || name === 'NaN') {
+    name = 'Аноним';
+  }
+
+  console.log(`Привет, мир! Меня зовут ${name}.`);
+  return `Привет, мир! Меня зовут ${name}.`;
 }
